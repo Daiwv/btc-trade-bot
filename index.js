e23bcd8e4b1975e703c2591984a6916293ab6108
John Kor <john.kor@gmail.com>
2015-11-14 15:49:42 -0800

    first attempt at creating a bot for btc trading and transfering between wallets. not fully sure yet what some of the api params are for kraken. will need more clarification

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 var config = require('./config')
+var async = require('async');
+
+var QuadrigaCX = require('quadrigacx');
+var quadrigacx = new QuadrigaCX(
+  config.quadrigacx.clientId,
+  config.quadrigacx.apiKey,
+  config.quadrigacx.apiSecret
+);
+
+var KrakenClient = require('kraken-api');
+var kraken = new KrakenClient(
+  config.kraken.apiKey,
+  config.kraken.privateKey
+);
+
+// kraken.api('Assets', {
+// }, function(error, data) {
+//   console.log(error, data)  
+// })
+
+// kraken.api('DepositMethods', {
+//   asset: 'XBT/EUR'
+// }, function(error, data) {
+//   console.log(error, data)  
+// })
+
+
+// kraken.api('DepositAddresses', {
+//   asset: 'XBT',
+//   method: ''
+// }, function(error, data) {
+//   console.log(error, data)  
+// })
+
+async.waterfall([
+  function buyInCanada(waterfallCb) {
+    quadrigacx.api('buy', {
+      amount: config.limitOrder.amount,
+      price: config.limitOrder.price
+    }, waterfallCb);
+  },
+  function getEuropeanWalletAddresss(waterfallCb) {
+    kraken.api('DepositAddresses', {
+      asset: '', // ???
+      method: '' // ???
+    }, function(error, data) {
+    })
+  },
+  function transferToEuropeanWallet(waterfallCb) {
+    quadrigacx.api('bitcoin_withdrawal', {
+      amount: '', //some amount to transfer
+      address: '' //address from above
+    }, waterfallCb);
+  },
+  function transferToEuropeanBank(waterfallCb) {
+    /// ???
+  }
+], function(err, result) {
+
+})
+
